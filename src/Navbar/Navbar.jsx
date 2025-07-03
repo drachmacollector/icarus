785c7e1e5556312d54c89b319c77da0af7f69117
pranjalbaghel1343 <pranjalbaghel1343@gmail.com>
2025-07-03 12:03:23 +0530

    added home button

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -8,31 +8,32 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-links">
-        <Link to="/News" className={`nav-link ${location.pathname === "/News" ? "active" : ""}`}>
-          News
-        </Link>
-        <Link to="/Flare" className={`nav-link ${location.pathname === "/Flare" ? "active" : ""}`}>
-          Solar Flares
-        </Link>
-        <Link to="/CmeTracker" className={`nav-link ${location.pathname === "/CmeTracker" ? "active" : ""}`}>
-          Coronal Mass Ejections (CME)
-        </Link>
-        <Link to="/HeatMapDashboard" className={`nav-link ${location.pathname === "/HeatMapDashboard" ? "active" : ""}`}>
-          Heat Map
-        </Link>
-        <Link to="/AuroraForecast" className={`nav-link ${location.pathname === "/risk" ? "active" : ""}`}>
-          Aurora
-        </Link>
-        <Link to="/Timeline" className={`nav-link ${location.pathname === "/Timeline" ? "active" : ""}`}>
-          Timeline
-        </Link>
-        <Link
-                  to="/status"
-                  className={`nav-link ${location.pathname === "/status" ? "active" : ""}`}
-                >
-                  🗺️ Status Map
-                </Link>
-      </div>
+  <Link to="/" className={`nav-link ${location.pathname === "/" ? "active" : ""}`}>
+    Home
+  </Link>
+  <Link to="/News" className={`nav-link ${location.pathname === "/News" ? "active" : ""}`}>
+    News
+  </Link>
+  <Link to="/Flare" className={`nav-link ${location.pathname === "/Flare" ? "active" : ""}`}>
+    Solar Flares
+  </Link>
+  <Link to="/CmeTracker" className={`nav-link ${location.pathname === "/CmeTracker" ? "active" : ""}`}>
+    Coronal Mass Ejections (CME)
+  </Link>
+  <Link to="/HeatMapDashboard" className={`nav-link ${location.pathname === "/HeatMapDashboard" ? "active" : ""}`}>
+    Heat Map
+  </Link>
+  <Link to="/AuroraForecast" className={`nav-link ${location.pathname === "/risk" ? "active" : ""}`}>
+    Aurora
+  </Link>
+  <Link to="/Timeline" className={`nav-link ${location.pathname === "/Timeline" ? "active" : ""}`}>
+    Timeline
+  </Link>
+  <Link to="/status" className={`nav-link ${location.pathname === "/status" ? "active" : ""}`}>
+    🗺️ Status Map
+  </Link>
+</div>
+
     </nav>
   );
 }
